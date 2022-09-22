cb343466b2a4044729df00ac30881ee9f114b7b9
Ayrton Diaz <ayrtondiazsalta@hotmail.com>
2022-09-21 21:43:03 -0300

    Desafio 3 fixed

--- Desafio3/index.js
+++ Desafio3/index.js
@@ -6,16 +6,15 @@ const PORT = 8080
 const products = new Contenedor('./products.txt')
 
 app.get('/productos', (req, res) => {
-  res.send(products.getAll())
+   return res.json(products.getAll())
 
 })
 app.get('/productosRandom', (req, res) => {
-    res.send(products.getRandom())  
+    return res.json(products.getRandom())  
   })
-  
 
 app.get('/', (req, res) => {
-  res.send('Hola Desafio')
+  return res.send('Hola Desafio')
 })
 
 app.listen(PORT, () => console.log('El servidor esta escuchando en puerto 8080'))
