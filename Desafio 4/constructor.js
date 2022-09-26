ee49377ce7115846545aa49c6d2ba3589f4dbdf9
Ayrton Diaz <ayrtondiazsalta@hotmail.com>
2022-09-26 20:21:41 -0300

    fixD4.1

---
+++ Desafio 4/constructor.js
@@ -0,0 +1,113 @@
+const fs = require('fs');
+
+class Container {
+    constructor(file) {
+        this.file = file;
+    }
+
+     #readFile() {
+        try {
+            const content =  fs.readFileSync(this.file, 'utf-8');
+            const parsedContent = JSON.parse(content);
+            return parsedContent;
+        } catch (error) {
+            return error;
+        }
+    }
+
+    async save(obj){ // guarda un objeto en el archivo, devuelve el id asignado
+        const contenidoArchivo =  await this.#readFile()
+        if (contenidoArchivo.length !== 0) {
+            console.log(contenidoArchivo)
+            await fs.promises.writeFile(this.file,JSON.stringify([...contenidoArchivo, {...obj, id: contenidoArchivo[contenidoArchivo.length - 1].id + 1}], null, 2), 'utf-8')
+        } else {            
+            await fs.promises.writeFile(this.file, JSON.stringify( [ {...obj, id: 1} ]), 'utf-8')
+        }
+
+    }
+
+    async modifyById(id, product) {
+        try {
+            let fileContent = await this.#readFile();
+            fileContent = await fileContent.map(e => {
+                if (e.id === id) {
+                    e = {
+                        ...product,
+                        id: id
+                    };
+                    return e;
+                } else {
+                    return e;
+                }
+            })
+            await fs.promises.writeFile(this.file, JSON.stringify(fileContent, null, 2), 'utf-8');
+
+            return 'Object updated.'
+        } catch (error) {
+            return error;
+        }
+    }
+
+     getById(id) {
+        const fileContent =  this.#readFile();
+        const element = fileContent.find(e => e.id === id)
+        return element ? element : 0;
+    }
+
+     getAll() {
+        const fileContent =  this.#readFile();
+        return fileContent;
+    }
+
+    async deleteById(id) {
+        try {
+            const fileContent = await this.#readFile();
+            const fileCopy = Array.from(fileContent);
+            const element = fileCopy.findIndex(obj => obj.id === id);
+            if (element >= 0) {
+                fileCopy.splice(element, 1);
+                await fs.promises.writeFile(this.file, JSON.stringify([...fileCopy], null, 2), 'utf-8');
+                return 'Element removed.';
+            } else {
+                return 'Element not found.';
+            }
+        } catch (error) {
+            return error;
+        }
+    }
+
+    async deleteAll() {
+        try {
+            const fileContent = await this.#readFile();
+            if (fileContent.length !== 0) {
+                await fs.promises.writeFile(this.file, JSON.stringify([]));
+                return 'Arrray cleared.'
+            } else {
+                return 'Array is already empty.';
+            }
+        } catch (error) {
+            return error;
+        }
+    }
+
+    getRandomProduct() {
+        try {
+            const fileContent =  this.#readFile();
+            const randomProduct = fileContent[Math.floor(Math.random() * fileContent.length)];
+            return randomProduct;
+        } catch (error) {
+            return error;
+        }
+    }
+}
+
+module.exports = Container;
+
+const contenedor = new Container('./prod.json')
+
+// contenedor.save({nombre: 'producto 4', precio: 100})
+// console.log("guardado")
+// contenedor.modifyById(1,{precio:150})
+
+// contenedor.deleteById(1)
+// console.log(contenedor.getAll())
